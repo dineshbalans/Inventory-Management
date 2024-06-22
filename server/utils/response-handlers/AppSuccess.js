93c93e377b6c4f4e4c5199b5b017d900508373c9
dineshbalans <dineshbalansrinivasan@gmail.com>
2024-06-22 16:55:19 +0530

    server: C -rh AppSuccess

---
+++ server/utils/response-handlers/AppSuccess.js
@@ -0,0 +1,10 @@
+class AppSuccess {
+    constructor(data, message, statusCode) {
+      this.statusCode = statusCode;
+      this.status = `${statusCode}`.startsWith('2') ? 'success' : 'valid';
+      this.message = message;
+      this.data = data != undefined ? data : null;
+    }
+  }
+  export default AppSuccess;
+  
