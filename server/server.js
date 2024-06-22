a443eed8f0054dfab1b0b81c498b8a8b7251b710
dineshbalans <dineshbalansrinivasan@gmail.com>
2024-06-22 17:02:56 +0530

    update: server.js

--- server/server.js
+++ server/server.js
@@ -1,16 +1,20 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const routes = require('./routes/routes');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const userRouter = require("./routes/userRoutes");
 
 const app = express();
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://localhost:27017/inventory', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.log(err));
+mongoose
+  .connect("mongodb://localhost:27017/inventory", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
-app.use('/api', routes);
+app.use("/api/v1/user", userRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
