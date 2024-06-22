771b67727acdf959a39c8d9e454518c5c978824b
dineshbalans <dineshbalansrinivasan@gmail.com>
2024-06-23 00:14:41 +0530

    client: C -api

---
+++ client/src/services/api.js
@@ -0,0 +1,55 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:5000/api";
+
+const register = (username, password) => {
+  return axios.post(`${API_URL}/register`, { username, password });
+};
+
+const login = (username, password) => {
+  return axios.post(`${API_URL}/login`, { username, password });
+};
+
+const addProduct = (token, product) => {
+  return axios.post(`${API_URL}/product`, product, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+};
+
+const updateProductQuantity = (token, productId, quantity) => {
+  return axios.put(
+    `${API_URL}/product`,
+    { productId, quantity },
+    { headers: { Authorization: `Bearer ${token}` } }
+  );
+};
+
+const getUserProducts = (token) => {
+  return axios.get(`${API_URL}/products`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+};
+
+const createInvoice = (token, products) => {
+  return axios.post(
+    `${API_URL}/invoice`,
+    { products },
+    { headers: { Authorization: `Bearer ${token}` } }
+  );
+};
+
+const getUserInvoices = (token) => {
+  return axios.get(`${API_URL}/invoices`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+};
+
+export {
+  register,
+  login,
+  addProduct,
+  updateProductQuantity,
+  getUserProducts,
+  createInvoice,
+  getUserInvoices,
+};
