76fd7dde660bef49f69db0c4570283b6cbadcc2c
dineshbalans <dineshbalansrinivasan@gmail.com>
2024-06-22 16:54:24 +0530

    server: C -rh AppError

---
+++ server/utils/response-handlers/AppError.js
@@ -0,0 +1,13 @@
+class AppError extends Error {
+    constructor(message, statusCode) {
+      message = typeof message == String ? message : JSON.stringify(message);
+      super(message);
+      this.statusCode = statusCode;
+      this.status = `${statusCode}`.startsWith("4") ? "fail" : "error";
+      this.isOperational = true;
+  
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+  export default AppError;
+  
